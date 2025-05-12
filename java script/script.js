bc7fa8569812a3737e51cd9ee685c136a52d1990
Akansha <aknshapal@gmail.com>
2025-05-12 11:24:59 +0530

    Delete java script directory

--- java script/script.js
+++
@@ -1,6 +0,0 @@
-const navElement = document.getElementById("navbar");
-const mobileMenuIcon = document.getElementById("mobile-menu");
-
-mobileMenuIcon.addEventListener("click", () =>
-  navElement.classList.toggle("active")
-);
